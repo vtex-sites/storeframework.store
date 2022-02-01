8241f72a4ad15a23f1a769dcca893665989d8690
igorbrasileiro <brasileiro456@gmail.com>
2022-02-01 11:51:06 -0300

    Remove unnecessary consts

--- sync-store-files.sh
+++ sync-store-files.sh
@@ -6,18 +6,19 @@ fastmod -m -d gatsby-config.js 'plugins: \[' 'plugins: [  { resolve: "@vtex/gats
 
 fastmod -m -d package.json '"name": "base.store"' '"name": "base-cms.store"';
 fastmod -m -d package.json '"@vtex/gatsby-plugin-thumbor": "\^(\d*.\d*.\d*)",' '"@vtex/gatsby-plugin-thumbor": "^${1}",
-    "@vtex/gatsby-source-cms": "^0.2.4",';
+    "@vtex/gatsby-plugin-cms": "^${1}",';
 
 # Update src/pages/index.tsx file
 
 ## imports
 
-fastmod -d src/pages/index.tsx "import\sBannerText.*|import\sHero.*" "";
+fastmod -d src/pages/index.tsx "import\s(BannerText|Hero|ProductTiles|ProductShelf|IncentivesHeader).*" "";
 fastmod -d src/pages/index.tsx "(import\sReact.*)" "\${1}
 import RenderCMS from 'src/components/RenderCMS'";
 
 ## Component
-fastmod -d src/pages/index.tsx 'data: \{ (.*) \},' 'data: { ${1}, cmsHome },';
+fastmod -d src/pages/index.tsx 'data: \{ (.*)(allStore.*)\}' 'data: { ${1} cmsHome }';
+fastmod -d src/pages/index.tsx "const\s(product|haveProduct).*" "";
 fastmod -m -d src/pages/index.tsx '(\{\w*\s&&\s\(\s)?<section.*section>(.*\)\})?$' '<RenderCMS sections={cmsHome?.sections} />';
 
 ## Query
