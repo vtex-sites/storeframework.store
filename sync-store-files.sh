4e1f46a63111dbf2abca5baf8f1cb2797a71b348
igorbrasileiro <brasileiro456@gmail.com>
2022-02-01 10:25:18 -0300

    Fix package.json update

--- sync-store-files.sh
+++ sync-store-files.sh
@@ -6,7 +6,7 @@ fastmod -m -d gatsby-config.js 'plugins: \[' 'plugins: [  { resolve: "@vtex/gats
 
 fastmod -m -d package.json '"name": "base.store"' '"name": "base-cms.store"';
 fastmod -m -d package.json '"@vtex/gatsby-plugin-thumbor": "\^(\d*.\d*.\d*)",' '"@vtex/gatsby-plugin-thumbor": "^${1}",
-    "@vtex/gatsby-plugin-cms": "^${1}"';
+    "@vtex/gatsby-source-cms": "^0.2.4",';
 
 # Update src/pages/index.tsx file
 
@@ -17,7 +17,7 @@ fastmod -d src/pages/index.tsx "(import\sReact.*)" "\${1}
 import RenderCMS from 'src/components/RenderCMS'";
 
 ## Component
-fastmod -d src/pages/index.tsx 'data: \{ (.*) \},' 'data: { ${1}, cmsHome }';
+fastmod -d src/pages/index.tsx 'data: \{ (.*) \},' 'data: { ${1}, cmsHome },';
 fastmod -m -d src/pages/index.tsx '(\{\w*\s&&\s\(\s)?<section.*section>(.*\)\})?$' '<RenderCMS sections={cmsHome?.sections} />';
 
 ## Query
